2b40296e95103f627b33e11851577137c8e0c1ac
Rick Mazur <113574185+rickmzr@users.noreply.github.com>
2023-06-08 07:40:55 -0500

    Build a React info site
    --Finish #27

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main() {
-  return <h1>Main goes here</h1>;
+  return (
+    <div className="main-container">
+      <h1 className="main-title">Fun facts about React</h1>
+      <ul className="main-facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
 }
